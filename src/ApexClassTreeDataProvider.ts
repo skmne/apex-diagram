a27a8c0e5db5b1eaa4d8b251c8361b235c58a851
alexander.lesik <skmne94@gmail.com>
2023-05-21 17:13:27 +0200

    Init tree view to navigate by classes

---
+++ src/ApexClassTreeDataProvider.ts
@@ -0,0 +1,59 @@
+import * as vscode from "vscode";
+
+export class ApexClassTreeDataProvider implements vscode.TreeDataProvider<ApexClassTreeItem> {
+	constructor(private workspaceRoot: string, private apexClassMembers: Array<any>) {}
+
+	private _onDidChangeTreeData: vscode.EventEmitter<ApexClassTreeItem | undefined | null | void> =
+		new vscode.EventEmitter<ApexClassTreeItem | undefined | null | void>();
+
+	readonly onDidChangeTreeData: vscode.Event<ApexClassTreeItem | undefined | null | void> =
+		this._onDidChangeTreeData.event;
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	refreshItem(node: ApexClassTreeItem): void {
+		this._onDidChangeTreeData.fire(node);
+	}
+
+	getTreeItem(element: ApexClassTreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: ApexClassTreeItem): Thenable<ApexClassTreeItem[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage("No sfdx connection in empty workspace");
+			return Promise.resolve([]);
+		}
+
+		return Promise.resolve(this.getApexClassTreeItems());
+	}
+
+	private getApexClassTreeItems(): ApexClassTreeItem[] {
+		const apexClassTreeItems = [];
+		for (const apexClass of this.apexClassMembers) {
+			apexClassTreeItems.push(
+				new ApexClassTreeItem(apexClass.name, apexClass.version, vscode.TreeItemCollapsibleState.None)
+			);
+		}
+		return apexClassTreeItems;
+	}
+}
+
+class ApexClassTreeItem extends vscode.TreeItem {
+	tooltip: string;
+	description: string;
+	iconPath = new vscode.ThemeIcon("outline-view-icon");
+	contextValue: string = "add_context";
+
+	constructor(
+		public readonly label: any,
+		private readonly version: any,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState
+	) {
+		super(label, collapsibleState);
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { ApexClassTreeDataProvider } from "./ApexClassTreeDataProvider";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const rootPath: any =
+		vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+			? vscode.workspace.workspaceFolders[0].uri.fsPath
+			: undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "apex-diagram" is now active!');
+	const data: Array<any> = [
+		{ name: "Account", version: "v54" },
+		{ name: "BoatDataService", version: "v54" },
+		{ name: "GenerateDataTests", version: "v54" },
+		{ name: "fflib_SObjectDescribe", version: "v54" },
+		{ name: "fflib_SObjectDomain", version: "v54" },
+		{ name: "fflib_ISelectorFactory", version: "v54" },
+		{ name: "fflib_Application", version: "v54" },
+		{ name: "fflib_ISObjects", version: "v54" },
+		{ name: "BaseApexClass", version: "v54" },
+		{ name: "fflib_SObjectSelector", version: "v54" },
+	];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('apex-diagram.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from apex-diagram!');
+	const apexClassesTreeProvider = new ApexClassTreeDataProvider(rootPath, data);
+	vscode.window.createTreeView("apex-classes-view", {
+		treeDataProvider: apexClassesTreeProvider,
+		canSelectMany: false,
 	});
 
-	context.subscriptions.push(disposable);
+	vscode.commands.registerCommand("apex-classes-view.refreshEntry", () => apexClassesTreeProvider.refresh());
+
+	vscode.commands.registerCommand("apex-classes-view.addEntry", (node: any) => {
+		node.contextValue = "remove_context";
+		apexClassesTreeProvider.refreshItem(node);
+		vscode.window.showInformationMessage(`Successfully added ${node.label}.`);
+	});
+	vscode.commands.registerCommand("apex-classes-view.removeEntry", (node: any) => {
+		node.contextValue = "add_context";
+		apexClassesTreeProvider.refreshItem(node);
+		vscode.window.showInformationMessage(`Successfully remove ${node.label}.`);
+	});
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
