09d46b29f80e615d039d86208c80d42c487cdbd5
alexander.lesik <skmne94@gmail.com>
2023-06-17 21:06:58 +0200

    Initialize the diagram workspace user interface

--- web/diagram-workspace/index.js
+++ web/diagram-workspace/index.js
@@ -1,14 +1,43 @@
-const counter = document.getElementById("lines-of-code-counter");
-			// Handle the message inside the webview
-			window.addEventListener("message", (event) => {
-				const message = event.data; // The JSON data our extension sent
-				console.log(message);
-				switch (message.command) {
-					case "Add":
-						counter.textContent = message.value;
-						break;
-					case "Remove":
-						counter.textContent = message.value;
-						break;
-				}
-			});
+// import Diagram from "./MyLibrary";
+
+var header = document.getElementById("header").getBoundingClientRect();
+var svgContainer = document.getElementById("container").getBoundingClientRect();
+var width = svgContainer.width;
+var height = svgContainer.height - header.height;
+const svgElement = document.querySelector("svg");
+
+setSvgSize(svgElement, width, height);
+window.addEventListener(
+	"resize",
+	(event) => {
+		header = document.getElementById("header").getBoundingClientRect();
+		svgContainer = document.getElementById("container").getBoundingClientRect();
+		width = svgContainer.width;
+		height = svgContainer.height - header.height;
+		setSvgSize(svgElement, width, height);
+	},
+	true
+);
+
+// Handle the message inside the webview
+window.addEventListener("message", (event) => {
+	const message = event.data; // The JSON data our extension sent
+	console.log(message);
+	switch (message.command) {
+		case "Add":
+			console.log("add");
+			break;
+		case "Remove":
+			console.log("remove");
+			break;
+	}
+});
+
+function setSvgSize(svgElement, width, height) {
+	if (svgElement) {
+		svgElement.setAttribute("width", width);
+		svgElement.setAttribute("height", height);
+	}
+}
+
+const diagram = new MyLibrary.default(svgElement);
