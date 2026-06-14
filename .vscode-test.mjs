e31fbe6e8529db8930c418990b9831755db462e5
Aliaksandr Lesik <alexander.lesik.pl@gmail.com>
2026-06-14 16:34:32 +0200

    Fix VS Code test runner output path

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from '@vscode/test-cli';
 export default defineConfig([
 	{
 		label: 'integrationTests',
-		files: 'out/test/suite/**/*.test.js',
+		files: 'out-test/test/suite/**/*.test.js',
 		version: 'insiders',
 		launchArgs: ['--user-data-dir', '.vscode-test/user-data'],
 		mocha: {
